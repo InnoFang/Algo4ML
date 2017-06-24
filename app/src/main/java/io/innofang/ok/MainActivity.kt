6e65ec127e6a58782e0736f789ac16beaba03c4f
InnoFang <q984941707@qq.com>
2017-06-24 12:46:29 +0800

    upload project

---
+++ app/src/main/java/io/innofang/ok/MainActivity.kt
@@ -0,0 +1,16 @@
+package io.innofang.ok
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private val textView: TextView by lazy { findViewById(R.id.text_view) as TextView }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
