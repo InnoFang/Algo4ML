8fe67fd38f5dde8cf18b3c5ac5025f266244faf8
InnoFang <innofang@outlook.com>
2020-06-10 20:28:50 +0800

    :art: update

--- MLiA/adaboost/adaboost.py
+++ MLiA/adaboost/adaboost.py
@@ -6,10 +6,16 @@ def loadSimpleData():
     简单数据集
     :return: 坐标，类别
     """
-    data_matrix = [[1., 2.1],
+    data_matrix = np.array([[1., 2.1],
                    [2., 1.1],
                    [1.3, 1.],
                    [1., 1.],
-                   [2., 1.]]
+                   [2., 1.]])
     class_labels = [1.0, 1.0, -1.0, -1.0, 1.0]
     return data_matrix, class_labels
+
+
+def stumpClassify(data_matrix, dimen, threshVal, threshIneq):
+    ret_array = np.ones((np.shape(data_matrix)[0]), 1)
+    if threshIneq == 'lt':
+        ret_array[data_matrix[:,]]
--- test/MLiA/adaboost/test.py
+++ test/MLiA/adaboost/test.py
@@ -1,28 +1,20 @@
 from MLiA.adaboost import adaboost
 import matplotlib.pyplot as plt
+import numpy as np
 import unittest
 
 
 class TestAdaBoost(unittest.TestCase):
     def test_SimpleData(self):
         data_matrix, class_labels = adaboost.loadSimpleData()
-        assert len(data_matrix) == len(class_labels)
-        data_len = len(data_matrix)
-        positive_X, positive_Y = [], []
-        negative_X, negative_Y = [], []
-        for i in range(data_len):
-            # label is positive
-            if class_labels[i] == 1.0:
-                positive_X.append(data_matrix[i][0])
-                positive_Y.append(data_matrix[i][1])
-            # label is negative
-            elif class_labels[i] == -1.0:
-                negative_X.append(data_matrix[i][0])
-                negative_Y.append(data_matrix[i][1])
+        class_labels = np.array(class_labels)
+        positive_axis = data_matrix[class_labels == 1.0]
+        negative_axis = data_matrix[class_labels == -1.0]
 
         plt.title("Single layer decision tree test data")
         plt.xlim(xmin=0.8, xmax=2.2)
         plt.ylim(ymin=0.8, ymax=2.2)
-        plt.scatter(positive_X, positive_Y, s=150, marker='.', c="blue")
-        plt.scatter(negative_X, negative_Y, s=150, marker='^', c="red")
+        plt.scatter(positive_axis[:, 0], positive_axis[:, 1], s=150, marker='.', c="blue")
+        plt.scatter(negative_axis[:, 0], negative_axis[:, 1], s=150, marker='^', c="red")
+        plt.plot()
         plt.show()
