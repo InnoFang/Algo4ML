1d2e1ac0b6af22431a9efe9bc0947870907ec8f2
InnoFang <innofang@outlook.com>
2020-06-15 11:05:42 +0800

    :art: implement ridge regression

--- MLiA/linear_regresssion/regression.py
+++ MLiA/linear_regresssion/regression.py
@@ -65,3 +65,37 @@ def rssError(yArr, yHatArr):
     :return:
     """
     return ((yArr - yHatArr) ** 2).sum()
+
+
+def ridgeRegress(xMat, yMat, lam=0.2):
+    """
+    岭回归计算公式：w^ = (X^T·X + λI)^-1 · X^T · y
+    :param xMat:
+    :param yMat:
+    :param lam: λ
+    :return:
+    """
+    xTx = xMat.T * xMat
+    denom = xTx + np.eye(np.shape(xMat)[1]) * lam
+    # 按理说岭回归不会导致行列式为 0 的情况，但仍需要检查，因为当 lam=0 时，岭回归就变成了普通回归
+    if np.linalg.det(denom) == 0.0:
+        print("This matrix is singular, cannot do inverse")
+        return
+    ws = denom.I * (xMat.T * yMat)
+    return ws
+
+
+def ridgeTest(xArr, yArr):
+    x_mat, y_mat = np.mat(xArr), np.mat(yArr)
+    y_mean = np.mean(y_mat, 0)
+    y_mat = y_mat - y_mean
+    x_means = np.mean(x_mat, 0)
+    x_var = np.var(x_mat, 0)
+    # 所有特征均减去各自的均值并除以方差
+    x_mat = (x_mat - x_means) / x_var
+    num_test_pts = 30
+    w_mat = np.zeros((num_test_pts, np.shape(x_mat)[1]))
+    for i in range(num_test_pts):
+        ws = ridgeRegress(x_mat, y_mat, np.exp(i - 10))
+        w_mat[i, :] = ws.T
+    return w_mat
