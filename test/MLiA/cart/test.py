a22c02793933d59f729cc08b70a4293224c3ac90
InnoFang <innofang@outlook.com>
2020-06-18 16:56:49 +0800

    :white_check_mark: test prepruning

--- test/MLiA/cart/test.py
+++ test/MLiA/cart/test.py
@@ -16,3 +16,17 @@ class TestCART(unittest.TestCase):
         my_mat = np.mat(my_data)
         my_tree = regTrees.createTree(my_mat)
         print(my_tree)
+
+    def test_prepruning(self):
+        my_data = dataset.load_ex2()
+        my_mat = np.mat(my_data)
+        my_tree = regTrees.createTree(my_mat)
+        # 输出很多叶节点
+        print(my_tree)
+
+        print("prepruning")
+
+        # 修改停止条件 tol_s，tol_s 对误差的数量级十分敏感
+        # 在选项中耗费时间并对误差容忍度去平方值，也能得到仅有两个叶节点组成的树
+        my_tree = regTrees.createTree(my_mat, ops=(10000, 4))
+        print(my_tree)
