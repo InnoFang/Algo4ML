31fe66d5123ca033825a75d49f35d00b867cc30d
InnoFang <innofang@outlook.com>
2020-06-21 18:27:09 +0800

    :white_check_mark: test SVM

---
+++ test/MLiA/svm/test.py
@@ -0,0 +1,12 @@
+from MLiA.svm import svm
+import numpy as np
+import unittest
+
+
+class TestSVM(unittest.TestCase):
+    def test_loadData(self):
+        data_arr, label_arr = svm.loadDataSet('data/testSet.txt')
+        # print(labelArr)
+
+        b, alphas = svm.smoSimple(data_arr, label_arr, 0.6, 0.001, 40)
+        print(b)
