5e658855cb4b5366e98260408af0f19854d760cd
InnoFang <innofang@outlook.com>
2020-06-22 10:51:30 +0800

    :white_check_mark: test simple smo

--- test/MLiA/svm/test.py
+++ test/MLiA/svm/test.py
@@ -8,5 +8,13 @@ class TestSVM(unittest.TestCase):
         data_arr, label_arr = svm.loadDataSet('data/testSet.txt')
         print(label_arr)
 
-        # b, alphas = svm.smoSimple(data_arr, label_arr, 0.6, 0.001, 40)
-        # print(b)
+    def test_smoSimple(self):
+        data_arr, label_arr = svm.loadDataSet('data/testSet.txt')
+        b, alphas = svm.smoSimple(data_arr, label_arr, 0.6, 0.001, 40)
+        print(b)
+        print(alphas[alphas > 0])
+        print("支持向量的个数：", np.shape(alphas[alphas > 0]))
+        print("哪些数据点是支持向量：", "data", "label")
+        for i in range(100):
+            if alphas[i] > 0.0:
+                print(data_arr[i], label_arr[i])
