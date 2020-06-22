7fc02244446bf0291ac59c11a65b6cdbe7902ad8
InnoFang <innofang@outlook.com>
2020-06-22 16:59:13 +0800

    :art: implement Platt SMO

--- MLiA/svm/svm.py
+++ MLiA/svm/svm.py
@@ -17,7 +17,7 @@ def loadDataSet(fileName):
     return data_mat, label_mat
 
 
-def selectJrand(i, m):
+def selectJRand(i, m):
     """
     用于在某个区间范围内随机选择一个整数
     :param i: 第一个 alpha 的下标
@@ -46,7 +46,7 @@ def clipAlpha(aj, H, L):
 
 
 """
-SMO 函数的伪代码大致如下：
+简单 SMO 函数的伪代码大致如下：
 
 创建一个 alpha 向量并将其初始化为 0 向量
 当迭代次数小于最大迭代次数时（外循环）：
@@ -99,7 +99,7 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
             if (label_mat[i] * Ei < -toler and alphas[i] < C) or (label_mat[i] * Ei > toler and alphas[i] > 0):
 
                 # 随机选择第二个 alpha 值，即 alpha[j]
-                j = selectJrand(i, m)
+                j = selectJRand(i, m)
                 fXj = float(np.multiply(alphas, label_mat).T * (data_matrix * data_matrix[j, :].T)) + b
                 Ej = fXj - float(label_mat[j])
                 alpha_i_old = alphas[i].copy()
@@ -160,3 +160,160 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
             iter = 0
         print("iteration number: %d" % iter)
     return b, alphas
+
+
+"""
+完整版的 Platt SMO 算法，相比于简单版的 SMO 算法，实现 alpha 的更改和代数运算的优化环节一模一样
+在优化过程中，唯一的不同就是选择 alpha 的方式
+完整版的 Platt SMO 算法应用了一些能够提速的启发方法，该算法是通过一个外循环来选择第一个 alpha 值的，
+并且选择过程会在两种方式之间交替：
+    一种方式是在所有数据集上进行单遍扫描；
+    另一种方式则是在非边界 alpha （不等于边界 0 或 C 的 alpha 值） 中实现单遍扫描
+"""
+
+
+class optStruct:
+    """
+    使用一个数据结构老保存所有的重要值，可以省掉很多手工输入的麻烦
+    """
+
+    def __init__(self, dataMatIn, classLabels, C, toler):
+        self.X = dataMatIn
+        self.label_mat = classLabels
+        self.C = C
+        self.tol = toler
+        self.m = np.shape(dataMatIn)[0]
+        self.alphas = np.mat(np.zero((self.m, 1)))
+        self.b = 0
+        # 误差缓存
+        self.e_cache = np.mat(np.zero((self.m, 2)))
+
+
+def calcEk(oS, k):
+    """
+    计算误差值
+    :param oS:
+    :param k:
+    :return:
+    """
+    fXk = float(np.multiply(oS.alphas, oS.label_mat).T * (oS.X * oS.X[k, :].T)) + oS.b
+    Ek = fXk - float(oS.label_mat[k])
+    return Ek
+
+
+def selectJ(i, oS, Ei):
+    """
+    用于选择第二个 alpha 值（或内循环的 alpha）
+    :param i:
+    :param oS:
+    :param Ei:
+    :return:
+    """
+    max_k, max_delta_e, Ej = -1, 0, 0
+    oS.e_cache[i] = [1, Ei]
+    valid_e_cache_list = np.nonzero(oS.e_cache[:, 0].A)[0]
+    if len(valid_e_cache_list) > 1:
+        for k in valid_e_cache_list:
+            if k == i:
+                continue
+            Ek = calcEk(oS, k)
+            delta_e = abs(Ei - Ek)
+            if delta_e > max_delta_e:
+                max_k, max_delta_e, Ej = k, delta_e, Ek
+        return max_k, Ej
+    else:
+        j = selectJRand(i, oS.m)
+        Ej = calcEk(oS, j)
+    return j, Ej
+
+
+def updateEk(oS, k):
+    """
+    计算误差值后放入误差缓存
+    :param oS:
+    :param k:
+    :return:
+    """
+    Ek = calcEk(oS, k)
+    oS.e_cache[k] = [1, Ek]
+
+
+def innerL(i, oS):
+    Ei = calcEk(i, oS)
+    if (oS.label_mat[i] * Ei < -oS.tol and oS.alphas[i] < oS.C) or (oS.label_mat[i] * Ei > oS.tol and oS.alphas[i] > 0):
+        j, Ej = selectJ(i, oS, Ei)
+        alpha_i_old, alpha_j_old = oS.alphas[i].copy(), oS.alphas[j].copy()
+        if oS.label_mat[i] != oS.label_mat[j]:
+            L = max(0, oS.alphas[j] - oS.alphas[i])
+            H = min(oS.C, oS.C + oS.alphas[j] - oS.alphas[i])
+        else:
+            L = max(0, oS.alphas[j] + oS.alphas[i] - oS.C)
+            H = min(oS.C, oS.alphas[j] + oS.alphas[i])
+        if L == H:
+            print("L==H")
+            return 0
+        eta = 2.0 * oS.X[i, :] * oS.X[j, :].T - oS.X[i, :] * oS.X[i, :].T - oS.X[j, :] * oS.X[j, :].T
+        if eta >= 0:
+            print("eta>=0")
+            return 0
+        oS.alphas[j] -= oS.label_mat[j] * (Ei - Ej) / eta
+        oS.alphas[j] = clipAlpha(oS.alphas[j], H, L)
+        updateEk(oS, j)
+        if abs(oS.alphas[j] - alpha_j_old) < 0.00001:
+            print("j not moving enough:")
+            return 0
+        oS.alphas[i] += oS.label_mat[j] * oS.label_mat[i] * (alpha_j_old - oS.alphas[j])
+        updateEk(oS, i)
+        b1 = oS.b - Ei - oS.label_mat[i] * (oS.alphas[i] - alpha_i_old) * oS.X[i, :] * oS.X[i, :].T - oS.label_mat[j] * \
+             (oS.alphas[j] - alpha_j_old) * oS.X[i, j] * oS.X[j, j].T
+        b2 = oS.b - Ej - oS.label_mat[i] * (oS.alphas[i] - alpha_i_old) * oS.X[i, :] * oS.X[j, :].T - oS.label_mat[j] * \
+             (oS.alphas[j] - alpha_j_old) * oS.X[j, j] * oS.X[j, j].T
+        if 0 < oS.alphas[i] < oS.C:
+            oS.b = b1
+        elif 0 < oS.alphas[i] < oS.C:
+            oS.b = b1
+        else:
+            oS.b = (b1 + b2) / 2.0
+        return 1
+    else:
+        return 0
+
+
+def smoPlatt(dataMatIn, classLabels, C, toler, maxIter, kTup=('lin', 0)):
+    """
+    完整的 Platt SMO 的外循环代码
+    :param dataMatIn:
+    :param classLabels:
+    :param C:
+    :param toler:
+    :param maxIter:
+    :param kTup:
+    :return:
+    """
+    oS = optStruct(
+        np.mat(dataMatIn),
+        np.mat(classLabels).transpose(),
+        C,
+        toler
+    )
+    iter = 0
+    entire_set, alpha_pairs_changed = True, 0
+    while iter < maxIter and (alpha_pairs_changed > 0 or entire_set):
+        alpha_pairs_changed = 0
+        if entire_set:
+            for i in range(oS.m):
+                alpha_pairs_changed += innerL(i, oS)
+                print("fullSet, iter: %d i:%d, pairs changed %d" % (iter, i, alpha_pairs_changed))
+            iter += 1
+        else:
+            non_bound_is = np.nonzero((oS.alphas.A > 0) * (oS.alphas.A < C))[0]
+            for i in non_bound_is:
+                alpha_pairs_changed += innerL(i, oS)
+                print("non-bound, iter: %d i:%d, pairs changed %d" % (iter, i, alpha_pairs_changed))
+            iter += 1
+        if entire_set:
+            entire_set = False
+        elif alpha_pairs_changed == 0:
+            entire_set = True
+        print("iteration number: %d" % iter)
+    return os.B, oS.alphas
