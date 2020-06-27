32cbdad695dfa27e5bee3917486f33a07b66773c
InnoFang <innofang@outlook.com>
2020-06-27 12:15:53 +0800

    :fire: clean directory

--- thoughtfulML/k_nearest_neighbors/dataset.py
+++
@@ -1,8 +0,0 @@
-import pkg_resources
-
-resource_package = __name__
-
-
-def load_king_county_data_geocoded():
-    filename = pkg_resources.resource_filename(resource_package, 'data/king_county_data_geocoded.csv')
-    return filename
