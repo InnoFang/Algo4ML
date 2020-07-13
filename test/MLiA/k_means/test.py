c264eb364b208de2d204280fe4da8a75a41a30db
InnoFang <innofang@outlook.com>
2020-07-13 12:06:26 +0800

    :white_check_mark: :art: test randCent function

---
+++ test/MLiA/k_means/test.py
@@ -0,0 +1,16 @@
+from MLiA.k_means import kMeans
+import numpy as np
+import unittest
+
+
+class TestKMeans(unittest.TestCase):
+    def test_randCent(self):
+        data_mat = np.mat(kMeans.loadDataSet('data/testSet.txt'))
+        print('min(data_mat[:, 0]) =', min(data_mat[:, 0]))
+        print('min(data_mat[:, 1]) =', min(data_mat[:, 1]))
+        print('max(data_mat[:, 0]) =', max(data_mat[:, 0]))
+        print('max(data_mat[:, 1]) =', max(data_mat[:, 1]))
+        centroids = kMeans.randCent(data_mat, 2)
+        print('centroids:', centroids)
+        print('kMeans.distEclud(data_mat[0], data_mat[1]):', kMeans.distEclud(data_mat[0], data_mat[1]))
+
