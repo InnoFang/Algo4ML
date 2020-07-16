11419a75fcc5b7aeac2da4eadbb405979da3fc07
InnoFang <innofang@outlook.com>
2020-07-16 13:29:24 +0800

    :bug: :art: update PCA

--- MLiA/pca/pca.py
+++ MLiA/pca/pca.py
@@ -4,13 +4,13 @@ import numpy as np
 def loadDataSet(fileName, delim='\t'):
     with open(fileName) as fr:
         string_arr = [line.strip().split(delim) for line in fr.readlines()]
-        data_arr = [map(float, line) for line in string_arr]
+        data_arr = [list(map(float, line)) for line in string_arr]
         return np.mat(data_arr)
 
 
 def pca(dataMat, topNfeat=9999999):
     # 去除平均值
-    mean_vals = np.mean(data_mat, axis=0)
+    mean_vals = np.mean(dataMat, axis=0)
     mean_removed = dataMat - mean_vals
     # 计算协方差矩阵
     cov_mat = np.cov(mean_removed, rowvar=False)
@@ -19,9 +19,9 @@ def pca(dataMat, topNfeat=9999999):
     # 将特征值从大到小排序
     eig_val_ind = np.argsort(eig_vals)
     # 保留最上面的 N 个特征
-    eig_val_ind = eig_val_ind[:-(topNfeat + 1):-1]
+    eig_val_ind = eig_val_ind[-1:-(topNfeat + 1):-1]
     red_eig_vects = eig_vects[:, eig_val_ind]
     # 将数据转换到上述 N 个特征向量构建的新空间中
     low_D_data_mat = mean_removed * red_eig_vects
     recon_mat = (low_D_data_mat * red_eig_vects.T) + mean_vals
-    return low_D_data_mat
+    return low_D_data_mat, recon_mat
