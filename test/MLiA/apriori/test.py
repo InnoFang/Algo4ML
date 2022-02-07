95ae50a4fe3e9db67ec4a9b03dfe67cd8ca79150
InnoFang <innofang@outlook.com>
2022-02-07 17:35:02 +0800

    :white_check_mark: test to generate rules

--- test/MLiA/apriori/test.py
+++ test/MLiA/apriori/test.py
@@ -16,3 +16,14 @@ class TestApriori(unittest.TestCase):
         l2, support_data = apriori.apriori(dataset, min_support=0.7)
         print('L(0.5):', l2)
         print('support_data(0.5):', support_data)
+
+    def test_generate_rules(self):
+        dataset = apriori.load_dataset()
+        print('dataset: ', dataset)
+
+        l1, support_data = apriori.apriori(dataset, min_support=0.5)
+        print('L(0.5):', l1)
+        print('support_data(0.5):', support_data)
+
+        rules = apriori.generate_rules(l1, support_data, min_conf=0.5)
+        print('rules:', rules)
