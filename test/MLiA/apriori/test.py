d0471ccd1f5aba9c8caa0dbff2dd9635a3aec062
InnoFang <innofang@outlook.com>
2022-02-07 17:31:42 +0800

    :while_check_mark: test apriori

--- test/MLiA/apriori/test.py
+++ test/MLiA/apriori/test.py
@@ -1,5 +1,18 @@
 import unittest
+from MLiA.apriori import apriori
+
 
 class TestApriori(unittest.TestCase):
-    def test_dataload(self):
-        pass
+    def test_apriori(self):
+        dataset = apriori.load_dataset()
+        print('dataset: ', dataset)
+
+        l1, support_data = apriori.apriori(dataset, min_support=0.7)
+        print('L(0.7):', l1)
+        print('support_data(0.7):', support_data)
+
+        print('->->->->->->->->->->->->->->->->->->->->->->->->->->->->')
+
+        l2, support_data = apriori.apriori(dataset, min_support=0.7)
+        print('L(0.5):', l2)
+        print('support_data(0.5):', support_data)
