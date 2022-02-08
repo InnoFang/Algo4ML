a01edba9e10faa7711a4d412bd3b4ee567a2f7dc
InnoFang <innofang@outlook.com>
2022-02-08 15:29:48 +0800

    :white_check_mark: test FP-growth

--- test/MLiA/fp_growth/test.py
+++ test/MLiA/fp_growth/test.py
@@ -24,3 +24,11 @@ class TestFPGrowth(unittest.TestCase):
         print(fp_growth.find_prefix_path('x', header_tab['x'][1]))
         print(fp_growth.find_prefix_path('z', header_tab['z'][1]))
         print(fp_growth.find_prefix_path('r', header_tab['r'][1]))
+
+    def test_create_cond_tree(self):
+        simple_data = fp_growth.load_simple_data()
+        init_set = fp_growth.create_init_set(simple_data)
+        fp_tree, header_tab = fp_growth.create_tree(init_set, 3)
+        freq_items = []
+        fp_growth.mine_tree(fp_tree, header_tab, 3, set([]), freq_items)
+        print(freq_items)
